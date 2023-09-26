06d05a804f2d98c579d4493ba9f7a26d4d875cda
郭君稳 <guojunwen111@126.com>
2023-09-26 17:47:52 +0800

    代理模式测试

--- src/main/java/org/example/api/agencymode/Client.java
+++ src/main/java/org/example/api/agencymode/Client.java
@@ -7,10 +7,14 @@ import java.lang.reflect.Proxy;
  * 客户端代码
  */
 public class Client {
+    /* public static void main(String[] args) {
+         Subject subject = new ProxySubject();
+         subject.request();
+     }*/
     public static void main(String[] args) {
         RealSubject realSubject = new RealSubject();
         InvocationHandler handler = new ProxyHandler(realSubject);
-
+        //测试
         Subject subject = (Subject) Proxy.newProxyInstance(
                 realSubject.getClass().getClassLoader(),
                 realSubject.getClass().getInterfaces(),
