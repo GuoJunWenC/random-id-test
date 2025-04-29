2e279c767e34ebc9598624ebf3c97f23ed01edd0
郭君稳 <guojunwen111@126.com>
2025-04-29 15:19:16 +0800

    feat:freemark导出

--- src/main/java/org/example/api/export/ExportController.java
+++ src/main/java/org/example/api/export/ExportController.java
@@ -26,24 +26,28 @@ public class ExportController {
         if (Objects.isNull(projectMonthReport)) {
             throw new RuntimeException("项目不存在");
         }
-/*
-        projectMonthReport.setDate(projectMonthReport.getReportDate().toString());
-*/
+        /*
+         * projectMonthReport.setDate(projectMonthReport.getReportDate().toString());
+         */
         Map<String, Object> map = new HashMap<>();
         List<ProjectExploitation> exploitationList = new ArrayList<>();
-        exploitationList.add(ProjectExploitation.builder().advanceSite("12").unitName("asdsawqeeqweqwewqwewqewqeEDWSWSWSWSWSWSWSWSWSWSFERW QWEDDWQ3EDRFEWfreckle非财务部规划 二刀肉访问发热体无4而发愁软文二锅头否4如染发给他房3让我给他发34 34温柔风 3丰田果 34儿童房个人43恶女个").build());
+        exploitationList.add(ProjectExploitation.builder().advanceSite("12").unitName(
+                "asdsawqeeqweqwewqwewqewqeEDWSWSWSWSWSWSWSWSWSWSFERW QWEDDWQ3EDRFEWfreckle非财务部规划 二刀肉访问发热体无4而发愁软文二锅头否4如染发给他房3让我给他发34 34温柔风 3丰田果 34儿童房个人43恶女个")
+                .build());
         exploitationList.add(ProjectExploitation.builder().advanceSite("11").unitName("哈哈哈").build());
         List<ProjectPipelineRelocation> list = new ArrayList<>();
-        list.add(ProjectPipelineRelocation.builder().designModification(1).unitName("asdsawqeeqweqwewqwewqewqe").designModificationComplete(3).build());
-        list.add(ProjectPipelineRelocation.builder().designModification(2).unitName("哈哈哈").designModificationComplete(4).build());
+        list.add(ProjectPipelineRelocation.builder().designModification(1).unitName("asdsawqeeqweqwewqwewqewqe")
+                .designModificationComplete(3).build());
+        list.add(ProjectPipelineRelocation.builder().designModification(2).unitName("哈哈哈").designModificationComplete(4)
+                .build());
         map.put("data", projectMonthReport);
         map.put("exploitationList", exploitationList);
         map.put("pipelineRelocationList", list);
         map.put("constructionProgressList", new ArrayList<>());
+        map.put("constructionProgressList", new ArrayList<>());
+        map.put("constructionProgressList", new ArrayList<>());
 
-        freemarkerUtil.createFreemarkerDoc(map,response, "projectReport.ftl");
+        freemarkerUtil.createFreemarkerDoc(map, response, "projectReport.ftl");
     }
-
-
 
 }
